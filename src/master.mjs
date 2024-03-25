cc7169c08054199b9e4f26db84b622da282a0474
Markus Felten <Markus.Felten@gmx.de>
2024-03-25 15:54:30 +0100

    feat: testRestoreVersion2

--- src/master.mjs
+++ src/master.mjs
@@ -1,4 +1,6 @@
 import { Master, SCHEMA_VERSION_CURRENT } from "@konsumation/model";
+import { createReadStream } from "node:fs";
+import { fileURLToPath } from "node:url";
 
 /**
  *
@@ -18,9 +20,42 @@ export async function testInitializeAndReopen(t, driver, options) {
   }
   t.deepEqual(categories, []);
 
-  master.close();
+  await master.close();
 
   const master2 = await driver.initialize(options);
   t.truthy(master2);
   t.is(master2.schemaVersion, SCHEMA_VERSION_CURRENT);
+  await master2.close();
+}
+
+export async function testRestoreVersion2(t, driver, options) {
+  const master = await driver.initialize(options);
+
+  const input = createReadStream(
+    fileURLToPath(new URL("fixtures/database-version-2.txt", import.meta.url)),
+    { encoding: "utf8" }
+  );
+  const { numberOfValues, numberOfCategories } = await master.restore(input);
+
+  t.is(numberOfCategories, 3);
+  t.is(numberOfValues, 3 * 10);
+
+  const categories = [];
+  for await (const c of master.categories()) {
+    categories.push(c);
+  }
+
+  t.deepEqual(
+    categories.map(c => c.name),
+    ["CAT-0", "CAT-1", "CAT-2"]
+  );
+
+  const meters = [];
+  for await (const m of categories[0].meters(master.db)) {
+    meters.push(m);
+  }
+  t.deepEqual(
+    meters.map(m => m.name),
+    ["M-0", "M-1"]
+  );
 }
