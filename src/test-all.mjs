631c5832880f69bc62c9a0a442659fb812fb6c06
Markus Felten <Markus.Felten@gmx.de>
2024-03-28 13:49:35 +0100

    fix: handle mappe attribute names while testing  attributes

--- src/test-all.mjs
+++ src/test-all.mjs
@@ -29,45 +29,56 @@ export async function testInitializeAndReopen(t, driver, options) {
   t.falsy(master2.context);
 }
 
-export function testCategoryConstructor(t, factory, extraValues) {
-  const values = {
-    name: "CAT-constructor",
-    description: "Category insert",
-    ...extraValues
-  };
+function testAttributes(t, factory, values) {
   const c = new factory(values);
-  for (const [k, v] of Object.entries(values)) {
-    t.is(c[k], v, `attribute ${k}`);
+
+  const inverseMapping = Object.fromEntries(
+    Object.entries(factory.attributeNameMapping).map(kv => [kv[1], kv[0]])
+  );
+
+  for (let [k, v] of Object.entries(values)) {
+    if (inverseMapping[k]) {
+      t.deepEqual(
+        c[inverseMapping[k]],
+        v,
+        `attribute ${k}(${inverseMapping[k]})`
+      );
+    } else {
+      t.deepEqual(c[k], v, `attribute ${k}`);
+    }
   }
+
+  values = Object.fromEntries(
+    Object.entries(values).filter(
+      kv => factory.attributeNameMapping[kv[0]] === undefined
+    )
+  );
+
   t.deepEqual(c.attributeValues, values);
 }
 
+export function testCategoryConstructor(t, factory, extraValues) {
+  testAttributes(t, factory, {
+    name: "CAT-constructor",
+    description: "Category insert",
+    ...extraValues
+  });
+}
+
 export function testMeterConstructor(t, factory, extraValues) {
-  const values = {
+  testAttributes(t, factory, {
     serial: "12345",
     description: `meter for category CAT1`,
     unit: "kwh",
     fractionalDigits: 2,
     validFrom: new Date(),
     ...extraValues
-  };
-
-  const c = new factory(values);
-  for (const [k, v] of Object.entries(values)) {
-    t.is(c[k], v, `attribute ${k}`);
-  }
-  t.deepEqual(c.attributeValues, values);
+  });
 }
 
 export function testNoteConstructor(t, factory, extraValues) {
-  const values = {
+  testAttributes(t, factory, {
     description: "Note insert",
     ...extraValues
-  };
-
-  const c = new factory(values);
-  for (const [k, v] of Object.entries(values)) {
-    t.is(c[k], v, `attribute ${k}`);
-  }
-  t.deepEqual(c.attributeValues, values);
+  });
 }
